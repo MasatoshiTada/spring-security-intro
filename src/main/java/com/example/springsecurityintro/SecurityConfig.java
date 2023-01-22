28369ac64e1e860cc73de22a257591beb0ad7bba
MasatoshiTada <rosso.e.nero84@gmail.com>
2023-01-22 15:59:33 +0900

    Spring Boot 3.0.2にアップデート

--- src/main/java/com/example/springsecurityintro/SecurityConfig.java
+++ src/main/java/com/example/springsecurityintro/SecurityConfig.java
@@ -3,12 +3,16 @@ package com.example.springsecurityintro;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
